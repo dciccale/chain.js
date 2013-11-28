46a63d13e3676ad0a8e07ff884456c1c6dab68af
Denis Ciccale <dciccale@gmail.com>
2013-11-28 21:45:49 +0100

    a big update

--- chain.js
+++ chain.js
@@ -1,92 +1,183 @@
-// make native js methods chainable
-var chain = $ = function (el) {
-  return new chain.prototype.init(el);
-},
-  document = document;
+/*!
+ * chain.js
+ * Copyright (c) 2013 Denis Ciccale (@tdecs)
+ * Released under the MIT license
+ * https://github.com/dciccale/chain.js/blob/master/LICENSE.txt
+ */
+(function (window) {
 
-chain.prototype = {
-  length: 0,
+  var ArrayProto = Array.prototype;
+  var push = ArrayProto.push;
+  var splice = ArrayProto.splice;
+  var forEach = ArrayProto.forEach;
 
-  init: function (el) {
-    if (!el) {
+  var document = window.document;
+
+  var isUndefined = function (value) {
+    return typeof value === 'undefined';
+  };
+
+  var isDefined = function (value) {
+    return typeof value !== 'undefined';
+  };
+
+  var isString = function (value) {
+    return typeof value === 'string';
+  };
+
+  var isChained = function (obj) {
+    return obj instanceof Chain;
+  };
+
+  // make native js methods chainable
+  var Chain = $ = function (selector) {
+    if (!isChained(this)) {
+      return new Chain(selector);
+    }
+
+    if (!selector) {
       return this;
     }
-    this.push.apply(this, el);
-  },
 
-  push: [].push,
-  splice: [].splice,
-  forEach: function (fn) {
-    [].forEach.call(this, fn);
-    return this;
-  }
-};
-
-chain.prototype.init.prototype = chain.prototype;
-
-var nativeSelectorMethods = ['getElementById', 'getElementsByClassName', 'getElementsByTagName', 'getElementsByName', 'querySelector', 'querySelectorAll'];
-
-nativeSelectorMethods.forEach(function (method) {
-  chain.prototype[method] = function (selector) {
-    var instance = $();
-    var elements = document[method](selector);
-    instance.push.apply(instance, (!elements.length ? [elements] : elements));
-    return instance;
-  }
-});
-
-var nativeMethods = ['addEventListener', 'removeEventListener'];
-
-nativeMethods.forEach(function (method) {
-  chain.prototype[method] = function () {
-    var args = arguments;
-    return this.forEach(function (el) {
-      el[method].apply(el, args);
-    });
-  }
-});
-
-
-var nativeProperties = ['style', 'value'];
-
-nativeProperties.forEach(function (property) {
-  chain.prototype[property] = function (key, value) {
-    if (key == null && value == null) {
-      return this[0][property];
+    if (isString(selector)) {
+      return rootContext.querySelectorAll(selector);
     }
-    return this.forEach(function (el) {
-      if (value == null) {
-        el[property] = key;
-      } else {
-        el[property][key] = value;
+
+    if (selector.nodeType) {
+      this[0] = selector;
+      this.length = 1;
+      return this;
+    }
+
+    return push.apply(this, selector);
+  };
+
+  Chain.prototype = {
+    length: 0,
+
+    splice: splice,
+
+    forEach: function (fn) {
+      forEach.call(this, fn);
+      return this;
+    }
+  };
+
+  var rootContext = Chain(document);
+
+  /*
+   * Native selector methods
+   */
+  var nativeSelectorMethods = [
+    'getElementById',
+    'getElementsByClassName',
+    'getElementsByTagName',
+    'getElementsByName',
+    'querySelector',
+    'querySelectorAll'
+  ];
+
+  nativeSelectorMethods.forEach(function (method) {
+    Chain.prototype[method] = function (selector) {
+      // no root element
+      if (!this[0] || !selector) {
+        return this;
       }
-    });
-  }
-});
 
-var elementProperties = ['innerHTML', 'outerHTML', 'textContent', 'innerText', 'outerText'];
+      return Chain(this[0][method](selector));
+    }
+  });
 
-elementProperties.forEach(function (property) {
-  chain.prototype[property] = function (value) {
-    if (value == null) {
-      return this[0][property];
-    } else {
+  /*
+   * Native DOM event methods
+   */
+  var nativeMethods = ['addEventListener', 'removeEventListener'];
+
+  nativeMethods.forEach(function (method) {
+    Chain.prototype[method] = function () {
+      var args = arguments;
+
       return this.forEach(function (el) {
-        el[property] = value;
+        el[method].apply(el, args);
       });
     }
-  }
-});
+  });
 
-// elementProperties that returns HTML collection or node
-var elementQueryProperties = ['firstElementChild', 'lastElementChild', 'nextElementChild', 'previousElementChild', 'children'];
-elementQueryProperties.forEach(function (property) {
-  chain.prototype[property] = function (value) {
-    var instance = $();
-    this.forEach(function (el) {
-      var elements = el[property];
-      instance.push.apply(instance, (!elements.length ? [elements] : elements));
-    });
-    return instance;
-  }
-});
+  /*
+   * Style property
+   */
+  Chain.prototype.style = function (key, value) {
+    // no element
+    if (!this[0]) {
+      return undefined;
+    }
+
+    // get
+    if (isDefined(key) && isUndefined(value)) {
+      return this[0].style[key];
+
+    // set
+    } else {
+      return this.forEach(function (el) {
+        el.style[key] = value;
+      });
+    }
+  };
+
+  /*
+   * DOM element properties
+   */
+  var elementProperties = [
+    'innerHTML',
+    'outerHTML',
+    'textContent',
+    'innerText',
+    'outerText',
+    'value'
+  ];
+
+  elementProperties.forEach(function (property) {
+    Chain.prototype[property] = function (value) {
+      // no element
+      if (!this[0]) {
+        return undefined;
+      }
+
+      // get
+      if (isUndefined(value)) {
+        return this[0][property];
+
+      // set
+      } else {
+        return this.forEach(function (el) {
+          el[property] = value;
+        });
+      }
+    }
+  });
+
+  /*
+   * DOM element properties that returns HTML collection or node
+   */
+  var elementQueryProperties = [
+    'firstElementChild',
+    'lastElementChild',
+    'nextElementChild',
+    'previousElementChild',
+    'children'
+  ];
+
+  elementQueryProperties.forEach(function (property) {
+    Chain.prototype[property] = function () {
+      // no element
+      if (!this[0]) {
+        return this;
+      }
+
+      return Chain(this[0][property]);
+    }
+  });
+
+  window.Chain = window.$ = Chain;
+
+}(window));
